b1aec58b04c2e501818bee279d95e5e24f137d0a
THOCED <thonon_cedric@hotmail.com>
2015-04-03 23:35:42 +0200

    modif

---
+++ src/code/DateHeureException.java
@@ -0,0 +1,19 @@
+package code;
+
+public class DateHeureException extends Exception 
+{
+
+	public DateHeureException(String arg0) 
+	{
+		super(arg0);
+		
+	}
+
+	@Override
+	public String getMessage() 
+	{
+		// TODO Auto-generated method stub
+		return super.getMessage();
+	}
+
+}
